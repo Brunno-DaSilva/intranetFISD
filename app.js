bc959b7003a4fdb2be97798c0483cf8eae157526
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-11-19 15:48:54 -0600

    Added imgs to banner bg from JS

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Ciao bella mi piacere parlare il italiano");
+
+const highlightImgBG = document.getElementById("banner__highlight_img");
+const alertOneImgBG = document.getElementById("banner__alert_one_img");
+const alertTwoImgBG = document.getElementById("banner__alert_two_img");
+
+console.log(highlightImgBG);
+
+highlightImgBG.style.backgroundImage =
+  "url('https://res.cloudinary.com/duprwuo4j/image/upload/v1637357990/kuanish-reymbaev-o_lLsdVTxak-unsplash_z53cvf.jpg')";
+alertOneImgBG.style.backgroundImage =
+  "url('https://res.cloudinary.com/duprwuo4j/image/upload/v1598505231/element5-digital-OyCl7Y4y0Bk-unsplash_x8xsvz.jpg')";
+alertTwoImgBG.style.backgroundImage =
+  "url('https://res.cloudinary.com/duprwuo4j/image/upload/v1598505052/cdc-GDokEYnOfnE-unsplash_1_fmcyhc.jpg')";
