cccec6c0b927d2885169113a81e3859f65f768be
brunno-dasilva <hi.dasilvab@gmail.com>
2022-01-18 11:54:47 -0600

    modal created for more bookmarks

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ console.log("Ciao bella mi piacere parlare il italiano");
 const highlightImgBG = document.getElementById("banner__highlight_img");
 const alertOneImgBG = document.getElementById("banner__alert_one_img");
 const alertTwoImgBG = document.getElementById("banner__alert_two_img");
+const qlinks__modal = document.getElementById("qlinks__modal");
+const modal__button = document.getElementById("qlinks__modal--button");
+const modal__button_close = document.getElementById(
+  "qlinks__modal--close-button"
+);
 
 highlightImgBG.style.backgroundImage =
   "url('https://res.cloudinary.com/duprwuo4j/image/upload/v1637357990/kuanish-reymbaev-o_lLsdVTxak-unsplash_z53cvf.jpg')";
@@ -11,28 +16,16 @@ alertOneImgBG.style.backgroundImage =
 alertTwoImgBG.style.backgroundImage =
   "url('https://res.cloudinary.com/duprwuo4j/image/upload/v1598505052/cdc-GDokEYnOfnE-unsplash_1_fmcyhc.jpg')";
 
-// Get the modal
-const qlinks__modal = document.getElementById("qlinks__modal");
-
-// Get the button that opens the modal
-const modal__button = document.getElementById("qlinks__modal--button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
+modal__button.onclick = function () {
+  qlinks__modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+modal__button_close.onclick = function () {
+  qlinks__modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == qlinks__modal) {
+    qlinks__modal.style.display = "none";
   }
 };
