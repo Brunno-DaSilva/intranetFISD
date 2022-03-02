4c213251e633436e0759dd8baad5e4aac20cf1fa
brunno-dasilva <hi.dasilvab@gmail.com>
2022-03-02 10:03:57 -0600

    add svg to data file

--- JS/get-image.js
+++ JS/get-image.js
@@ -1,45 +1,72 @@
-import { icons } from "./icons-data";
+import { ICONS_DATA } from "./icons-data.js";
 
 const app = document.querySelector("#random-img");
 const fetchedURL = `https://jsonplaceholder.typicode.com/users`;
-console.log("ICONS DATA: ", icons);
+console.log("ICONS DATA: ", ICONS_DATA);
 
-// function getData(url) {
-//   fetch(url)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((jsonData) => {
-//       app.innerHTML =
-//         "<div class='cards-container'>" +
-//         jsonData
-//           .map((data) => {
-//             return `
-//       <div class="articles__card">
-//             <div class="articles__card-img">
-//               <div class="svg_holder svg_holder-green">
-//               <img src='${data.url}' alt='${data.id}' />
-//               </div>
-//             </div>
-//             <div class="articles__card-title">
-//               <h3>Community Facility Use and Rental</h3>
-//             </div>
-//             <div class="articles__card-text">
-//               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-//                 Officia hic cumque nobis distinctio.</p>
-//             </div>
-//             <div class="articles__card-btn">
-//               <button>Read more</button>
-//             </div>
-//           </div>
-//           `;
-//           })
-//           .join("") +
-//         "</div>";
-//     })
-//     .catch((error) => {
-//       console.log("This is an error", error);
-//     });
-// }
+function pushDataToHTML(ICONS_DATA) {
+  app.innerHTML = ICONS_DATA.map((data) => {
+    return `
+<div class="articles__card">
+  <div class="articles__card-img">
+    <div class="svg_holder svg_holder-green">
+    <img style="border-radius: 60%" src='${data.url}' alt='${data.id}' />
+    </div>
+  </div>
+  <div class="articles__card-title">
+    <h3>Community Facility Use and Rental</h3>
+  </div>
+  <div class="articles__card-text">
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Officia hic cumque nobis distinctio.</p>
+  </div>
+  <div class="articles__card-btn">
+    <button>Read more</button>
+  </div>
+</div>
+`;
+  });
+}
+pushDataToHTML(ICONS_DATA);
 
-// getData(fetchedURL);
+/*********************************
+Fetch version
+function getData(url) {
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((jsonData) => {
+      app.innerHTML =
+        "<div class='cards-container'>" +
+        jsonData
+          .map((data) => {
+            return `
+      <div class="articles__card">
+            <div class="articles__card-img">
+              <div class="svg_holder svg_holder-green">
+              <img src='${data.url}' alt='${data.id}' />
+              </div>
+            </div>
+            <div class="articles__card-title">
+              <h3>Community Facility Use and Rental</h3>
+            </div>
+            <div class="articles__card-text">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Officia hic cumque nobis distinctio.</p>
+            </div>
+            <div class="articles__card-btn">
+              <button>Read more</button>
+            </div>
+          </div>
+          `;
+          })
+          .join("") +
+        "</div>";
+    })
+    .catch((error) => {
+      console.log("This is an error", error);
+    });
+}
+
+getData(fetchedURL);*/
--- JS/icons-data.js
+++ JS/icons-data.js
@@ -1,15 +1,18 @@
-export const icons = [
+export const ICONS_DATA = [
   {
     id: 1,
     url: "https://res.cloudinary.com/duprwuo4j/image/upload/c_scale,w_250/v1645637853/starwarsBG/YODA.jpg",
+    svg: "../img/icons/at.svg",
   },
   {
     id: 2,
     url: "https://res.cloudinary.com/duprwuo4j/image/upload/c_scale,w_250/v1645637854/starwarsBG/BB8.jpg",
+    svg: "../img/icons/apple.svg",
   },
   {
     id: 3,
     url: "https://res.cloudinary.com/duprwuo4j/image/upload/c_scale,w_250/v1645637853/starwarsBG/BOBAFET.jpg",
+    svg: "../img/icons/3d-cube-sphere.svg",
   },
   {
     id: 4,
